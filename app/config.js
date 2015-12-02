483e200716fcbfae28a1fc4ad545dfbea655094e
unknown <jlero.mail@gmail.com>
2015-12-02 18:59:25 +0200

    test div in mainstage
    testing continues
    
    testing intensifies
    
    testing intensifies2
    
    testing intensifies3
    
    testing intensifies4
    
    testing intensifies5
    
    testing intensifies6
    
    testing intensifies7
    
    testing intensifies8
    
    testing intensifies9
    
    testing intensifies10
    
    testing intensifies11
    
    testing intensifies12
    
    testing intensifies13
    
    testing intensifies14
    
    testing intensifies14
    
    testing intensifies15
    
    testing intensifies15
    
    testing intensifies15
    
    testing intensifies15
    
    testing intensifies16
    
    testing intensifies17
    
    size change
    
    logo
    
    size+pdf
    
    size
    
    size
    
    changes
    
    changes
    
    changes
    
    changes
    
    changes
    
    changes
    
    changes
    
    changes
    
    changes
    
    changes
    
    changes
    
    changes
    
    changes
    
    changes
    
    changes
    
    changes
    
    changes
    
    changes
    
    changes
    
    changes
    
    version 1.0.0
    
    size 30%
    
    size 30%
    
    size 30%

--- app/config.js
+++ app/config.js
@@ -38,7 +38,7 @@ app.cfg = {
 			id: "side",
 			thumbnail: "resources/tpl/builder/icons/builder-layout-side.png",
 			liveApp: "http://links.esri.com/storymaps/map_journal_example_side_panel",
-			sizes: { small: '30%', medium: '40%', large: '50%' },
+			sizes: { small: '25%', medium: '30%', large: '50%' },
 			positions: ["left", "right"],
 			themes: [
 				{name: "side-default-1", themeMajor: "white", dotNav: "#777777", panel: "#FFFFFF", media: "#EEEEEE", text: "#000000", textLink: "#555", softText: "#c0c0c0", softBtn: "#444",    esriLogo: "black"},
@@ -53,7 +53,7 @@ app.cfg = {
 			id: "float",
 			thumbnail: "resources/tpl/builder/icons/builder-layout-float.png",
 			liveApp: "http://links.esri.com/storymaps/map_journal_example_floating_panel",
-			sizes: { small: '25%', medium: '35%', large: '45%' },
+			sizes: { small: '22%', medium: '30%', large: '40%' },
 			positions: ["right", "left"],
 			themes: [
 				{name: "float-default-1", themeMajor: "black", dotNav: "#000000", panel: "#000000", media: "#000000", text: "#FFFFFF", textLink: "#DDD", softText: "#FFF",    softBtn: "#AAA", esriLogo: "white"}
